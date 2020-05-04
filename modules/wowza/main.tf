30cffd1cb234eceebe0edf23c39b1611711ecef5
Ryan Bartram <ryan@bartram.ch>
2020-05-04 17:54:04 +0100

    debug

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "wowza" {
   name                 = "wowza"
   resource_group_name  = azurerm_resource_group.wowza.name
   virtual_network_name = azurerm_virtual_network.wowza.name
-  address_prefix       = var.address_space
+  address_prefixes     = [var.address_space]
 
   enforce_private_link_endpoint_network_policies = true
 }
