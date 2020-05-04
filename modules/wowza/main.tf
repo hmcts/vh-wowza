2d58578ea918aaf50a21a3e899ac428bf4a4f186
Ryan Bartram <ryan@bartram.ch>
2020-05-04 18:18:17 +0100

    add deployment

--- modules/wowza/main.tf
+++ modules/wowza/main.tf
@@ -148,18 +148,6 @@ resource "azurerm_network_security_group" "wowza" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1050
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 resource "azurerm_network_interface" "wowza" {
