df2e11bbf9e0d727896e8aac2c5ce9e7c558c784
Ryan Bartram <ryan@bartram.ch>
2020-10-05 23:14:51 +0100

    update schema to match 0.13

--- modules/wowza/_versions.tf
+++ modules/wowza/_versions.tf
@@ -1,8 +1,17 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    azurerm  = "= 2.15.0"
-    template = "~> 2.1"
-    random   = ">= 2"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "= 2.15.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2"
+    }
   }
 }
