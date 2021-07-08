7a392b66d43471ab1820979c75c761009c693b68
lowflying <chriscm.neu@gmail.com>
2021-07-08 11:47:31 +0100

    marked passwords as sensitive

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
 output "rest_password" {
   description = ""
   value       = module.wowza.rest_password
+  sensitive = true
 }
 
 output "stream_password" {
   description = ""
   value       = module.wowza.stream_password
+  sensitive = true
 }
