166c42adfc26635c6b9cd008b8a574c8ed78ffeb
Your Name <james.ferrari@version1.com>
2022-10-21 12:26:33 +0100

    make KV user a reader

--- modules/wowza/key_vault.tf
+++ modules/wowza/key_vault.tf
@@ -2,3 +2,9 @@ data "azurerm_key_vault" "vhcoreinfra" {
   name                = "vhcoreinfra${var.environment}"
   resource_group_name = "vh-core-infra-${var.environment}"
 }
+
+resource "azurerm_role_assignment" "kv_user" {
+  scope                = data.azurerm_key_vault.vhcoreinfra.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.kv_user.object_id
+}
